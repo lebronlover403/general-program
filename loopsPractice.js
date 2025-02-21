3bc1f2c49f2e0b342370435c0a4998e93dcdc049
Smith <26SmithC@badinhs.org>
2025-02-21 13:45:51 -0500

    did loop practice

---
+++ loopsPractice.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 23; i++) {
+         if (i % 2 == 0){
+             continue;
+         }
+         console.log(i);
+ }
+ for (let i = 0; i < 3; i++) { 
+    alert( `number ${i}!` ); 
+}
+
